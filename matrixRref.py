e75e554fd4a0b60293369b3f59e7215ad2b66f0b
aromik <awillcarr@gmail.com>
2024-09-22 15:24:04 -0400

    made matrix rref fully functional

--- matrixRref.py
+++ matrixRref.py
@@ -10,6 +10,12 @@ def clip_text(text: str = "this was copied"):
 	clipper.update() # now it stays on the clipboard after the window is closed
 	clipper.destroy()
 
+"""
+stringify takes in a matrix(2d array of ints), and returns a string for wolfram alpha
+"""
+def stringify(matrix): 
+	return ("rref " + str(matrix)).replace("[", "{").replace("]", "}")
+
 pygame.init()
 pygame.font.init()
 pygame.display.set_caption("Matrix RREF")
@@ -20,7 +26,7 @@ screen = pygame.display.set_mode(screen_res)
 
 BACKGROUND = (0,63,191)
 
-inputBox = ti.TextInput()
+inputBox = ti.TextInput(placeholder = "Enter in number of rows")
 inputBox.rect.center = tuple(x // 2 for x in screen_res)
 
 matrix = []
@@ -32,25 +38,32 @@ while on:
 	for e in pygame.event.get():
 		if e.type == pygame.QUIT:
 			on = False
-		if e.type == pygame.KEYDOWN:
+		if not finishedMatrix and e.type == pygame.KEYDOWN:
 			if e.key == pygame.K_BACKSPACE:
 				inputBox.backspace()
 			elif e.unicode == " ":
 				inputBox.negate()
 			elif e.key == pygame.K_RETURN or e.key == pygame.K_KP_ENTER:
+				print(f'number: {inputBox.text}\nmatrix: {matrix}')
 				if not inputBox.isEmpty() and not finishedMatrix:
 					val = int(inputBox.text)
 
-					if i == -2 and val > 0:
-						rows = val
-						i = -1
-						print("i-2")
-					elif i == -1 and val > 0:
-						columns = val
-						matrix = [[] for _ in range(rows)]
-						i = 0
-						print("i-1")
-					elif i < rows:
+					if i == -2:
+						inputBox.text = "Enter in number of rows"
+						if val > 0: # split up so that program doesnt move on until columns and rows have positive values
+							rows = val
+							i = -1
+							print("i-2")
+							inputBox.text = "Enter in number of columns"
+					elif i == -1:
+						inputBox.text = "Enter in number of columns"
+						if val > 0:
+							columns = val
+							matrix = [[] for _ in range(rows)]
+							i = 0
+							print("i-1")
+							inputBox.text = f"Enter value for row {i+1}, column {j+1}"
+					elif i < rows and i >= 0:
 						# i < rows, j < columns
 						print("in range")
 						matrix[i].append(val)
@@ -60,11 +73,19 @@ while on:
 							j = 0
 							i += 1
 							print("j too big")
-					else:
-						finishedMatrix = True
 
-				print(f'number: {inputBox.text}\nmatrix: {matrix}')
+						if i >= rows:
+							finishedMatrix = True
+							inputBox.text = ''
+							string = stringify(matrix)
+							print(string)
+							clip_text(string)
+						else:
+							inputBox.text = f"Enter value for row {i+1}, column {j+1}"
+
+				inputBox.update()
 				inputBox.clear()
+				inputBox.modified = False
 			else:
 				# print(f"\nkey: {e.unicode}")
 				inputBox.charAdd(e.unicode)
